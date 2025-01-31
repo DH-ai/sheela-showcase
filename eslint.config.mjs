06c6cde4732bbdc7e00ff874860ad85dafe2bdeb
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2025-01-31 19:26:22 +0530

    working on the app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,21 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import prettier from 'eslint-config-prettier';
-
+import reactPlugin from 'eslint-plugin-react';
 /** @type {import('eslint').Linter.Config}*/
 
 
 export default [ 
 	js.configs.recommended,
+    {
+        ...reactPlugin.configs.flat.recommended,
+        settings:{
+            react:{
+                version: 'detect'
+            }
+        }
+    },
+    reactPlugin.configs.flat["jsx-runtime"],
 	{
 		files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
         languageOptions: {
@@ -16,6 +25,10 @@ export default [
             ecmaFeatures: {
                 jsx: true,
             },
+        },
+        rules: {
+            'react/no-unescaped-entities': 'off',
+            'react/prop-types': 'off',
         },
     },
 	prettier // always put it last
