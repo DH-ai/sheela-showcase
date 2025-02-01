bcc6e5efd009100968947dc5b60d68c351817110
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2025-02-01 20:48:07 +0530

    config tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-
+import './styles/output.css';
 const App = () => {
   return (
     <div>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}",".src/index.html","./src/componenets/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
