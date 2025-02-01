10e7ea0b67a07de491293acdb477876eae2596f3
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2025-02-02 01:04:59 +0530

    added things to env

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAJXV3rN_dmsLldKlhz0R1VAs9NSasMfJY",
-  authDomain: "sheela-showcase.firebaseapp.com",
-  projectId: "sheela-showcase",
-  storageBucket: "sheela-showcase.firebasestorage.app",
-  messagingSenderId: "16489798240",
-  appId: "1:16489798240:web:201341b6e67152ae0b8105",
-  measurementId: "G-V16C4Q9J0T"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
